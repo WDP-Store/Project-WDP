aff902156f6068214cdfbba99fce1078a10c4785
Minh Zug <minhnguyenhhhhn@gmail.com>
2024-06-30 15:58:50 +0700

    Fix ui order

--- fe/src/admin/pages/OrdersManager.js
+++ fe/src/admin/pages/OrdersManager.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Col, Form, Row, Table } from 'react-bootstrap';
-import Paginate from '../components/Paginate';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Badge from 'react-bootstrap/Badge';
-import Modal from 'react-bootstrap/Modal';
-import { toast } from 'react-toastify';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Col, Form, Row, Table } from "react-bootstrap";
+import Paginate from "../components/Paginate";
+import InputGroup from "react-bootstrap/InputGroup";
+import Badge from "react-bootstrap/Badge";
+import Modal from "react-bootstrap/Modal";
+import { toast } from "react-toastify";
 import { AiFillCaretRight } from "react-icons/ai";
 import { date } from "yup";
 import axios from "axios";
 
 export default function OrdersManager() {
-
   const [orders, setOrders] = useState([]); //fetched orders
   const [status, setStatus] = useState([]); //fetched status
   const [lgShow, setLgShow] = useState(false); //modal
@@ -27,7 +26,7 @@ export default function OrdersManager() {
     preparing: "warning",
     successful: "success",
     failed: "danger",
-  };  // each status has it color
+  }; // each status has it color
   const colorBadge = {
     preparing: "#9ea954db",
     pending: "#3876a9",
@@ -35,15 +34,15 @@ export default function OrdersManager() {
     preparing: "#9ea954db",
     successful: "#248e5cd6",
     failed: "#7c3c3cd6",
-  };  // each status has it color
+  }; // each status has it color
   const [currentDetail, setCurrentDetail] = useState([]);
   const user = JSON.parse(localStorage.getItem("data"));
 
   //for filtering
-  const [statusFilter, setStatusFilter] = useState('');
-  const [orderIdFilter, setOrderIdFilter] = useState('');
-  const [fromDate, setfromDate] = useState('');
-  const [toDate, settoDate] = useState('');
+  const [statusFilter, setStatusFilter] = useState("");
+  const [orderIdFilter, setOrderIdFilter] = useState("");
+  const [fromDate, setfromDate] = useState("");
+  const [toDate, settoDate] = useState("");
 
   const [refresh, setRefresh] = useState(true);
   //pagination
@@ -67,16 +66,13 @@ export default function OrdersManager() {
   };
   //
 
-  useEffect(
-    () => {
-      fetch(`https://wdp.bachgiaphat.vn/status`)
-        .then(res => res.json())
-        .then(json => {
-          setStatus(json);
-        }
-        );
-    }, []
-  );
+  useEffect(() => {
+    fetch(`https://wdp.bachgiaphat.vn/status`)
+      .then((res) => res.json())
+      .then((json) => {
+        setStatus(json);
+      });
+  }, []);
 
   const openDetail = (index) => {
     // fetch(`https://wdp.bachgiaphat.vn/order/` + id)
@@ -88,42 +84,42 @@ export default function OrdersManager() {
     setCurrentDetail(orders[index]);
   };
 
-  useEffect(() => { //filter with status, order id
+  useEffect(() => {
+    //filter with status, order id
     filterOrder(currentPage);
-  }, [currentPage, orderIdFilter, statusFilter, fromDate, toDate, refresh]
-  );
+  }, [currentPage, orderIdFilter, statusFilter, fromDate, toDate, refresh]);
 
   const filterOrder = (page) => {
     // var url = (`https://wdp.bachgiaphat.vn/orders/all?page=1&user=${user._id}`);
     // var url = (`https://wdp.bachgiaphat.vn/orders/all?page=1`);
-    var url = (`https://wdp.bachgiaphat.vn/orders/all?`);
-    console.log("data order:")
-    if (fromDate === '' && toDate === '') url += `&page=${page}`;
+    var url = `https://wdp.bachgiaphat.vn/orders/all?`;
+    console.log("data order:");
+    if (fromDate === "" && toDate === "") url += `&page=${page}`;
 
-    if (statusFilter !== '') {
-      url += ('&status=' + statusFilter);
+    if (statusFilter !== "") {
+      url += "&status=" + statusFilter;
     }
-    if (orderIdFilter !== '') {
-      url += ('&id=' + orderIdFilter);
+    if (orderIdFilter !== "") {
+      url += "&id=" + orderIdFilter;
     }
 
-    axios.get(url)
-      .then(res => {
-        console.log("data order:", res)
+    axios
+      .get(url)
+      .then((res) => {
+        console.log("data order:", res);
         setTotalPages(res.data.totalPages);
         setOrders(res.data.docs);
         return res.data.docs;
-      }
-      )
-      .then(json => {
+      })
+      .then((json) => {
         console.log("jsonjsonjsonjson");
         console.log(json);
-        if (fromDate !== '') {
+        if (fromDate !== "") {
           let temp = [...json];
-          temp = temp.filter((o) => (new Date(o.date)) >= (new Date(fromDate)));
+          temp = temp.filter((o) => new Date(o.date) >= new Date(fromDate));
           setOrders(temp);
         }
-        if (toDate !== '') {
+        if (toDate !== "") {
           let temp = [...json];
           temp = temp.filter((o) => new Date(o.date) <= new Date(toDate));
           setOrders(temp);
@@ -132,75 +128,80 @@ export default function OrdersManager() {
       .catch((err) => toast.error(err));
   };
 
-
-const updateStatus = (value, index, id) => {
-    console.log("status: ", value)
-    console.log("status: ", typeof value)
-    axios.put(`https://wdp.bachgiaphat.vn/orders/${id}`, {
-      ...orders[index],
-      status: value,
-    })
-    .then(() => {
-      const updatedOrders = [...orders];
-      updatedOrders[index].status._id = value;
-      setOrders(updatedOrders);
-      setRefresh(!refresh);
-      toast.success("Order status updated successfully");
-    })
-    .catch((err) => toast.error(err));
+  const updateStatus = (value, index, id) => {
+    console.log("status: ", value);
+    console.log("status: ", typeof value);
+    axios
+      .put(`https://wdp.bachgiaphat.vn/orders/${id}`, {
+        ...orders[index],
+        status: value,
+      })
+      .then(() => {
+        const updatedOrders = [...orders];
+        updatedOrders[index].status._id = value;
+        setOrders(updatedOrders);
+        setRefresh(!refresh);
+        toast.success("Order status updated successfully");
+      })
+      .catch((err) => toast.error(err));
   };
 
   return (
     <Container lg={10}>
       <h3 className="mt-2">My orders</h3>
-      <Row className='my-4'>
+      <Row className="my-4">
         <Col xs={6} md={2}>
           <InputGroup className="mb-3">
-            <InputGroup.Text>
-              Status
-            </InputGroup.Text>
-            <Form.Select onChange={(e) => setStatusFilter(e.target.value)}>
-              <option value={''}>
-                all
-              </option>
-              {status?.map((s) =>
+            <InputGroup.Text>Status</InputGroup.Text>
+            <Form.Select
+              onChange={(e) => {
+                setStatusFilter(e.target.value);
+                setCurrentPage(1);
+              }}
+            >
+              <option value={""}>all</option>
+              {status?.map((s) => (
                 <option key={s._id} value={s._id}>
                   {s.name}
                 </option>
-              )}
+              ))}
             </Form.Select>
           </InputGroup>
         </Col>
         <Col xs={6} md={2}>
           <InputGroup className="mb-3">
-            <InputGroup.Text>
-              Order id
-            </InputGroup.Text>
-            <Form.Control onChange={(e) => setOrderIdFilter(e.target.value)}>
-
-            </Form.Control>
+            <InputGroup.Text>Order id</InputGroup.Text>
+            <Form.Control
+              onChange={(e) => {
+                setOrderIdFilter(e.target.value);
+                setCurrentPage(1);
+              }}
+            ></Form.Control>
           </InputGroup>
         </Col>
-        <Col xs={12} md={2}>
-        </Col>
+        <Col xs={12} md={2}></Col>
         <Col xs={6} md={3}>
           <InputGroup className="mb-3">
-            <InputGroup.Text>
-              From
-            </InputGroup.Text>
-            <Form.Control type="date" onChange={(e) => setfromDate(e.target.value)}>
-
-            </Form.Control>
+            <InputGroup.Text>From</InputGroup.Text>
+            <Form.Control
+              type="date"
+              onChange={(e) => {
+                setfromDate(e.target.value);
+                setCurrentPage(1);
+              }}
+            ></Form.Control>
           </InputGroup>
         </Col>
         <Col xs={12} md={3}>
           <InputGroup className="mb-3">
-            <InputGroup.Text>
-              to
-            </InputGroup.Text>
-            <Form.Control type="date" onChange={(e) => settoDate(e.target.value)}>
-
-            </Form.Control>
+            <InputGroup.Text>to</InputGroup.Text>
+            <Form.Control
+              type="date"
+              onChange={(e) => {
+                settoDate(e.target.value);
+                setCurrentPage(1);
+              }}
+            ></Form.Control>
           </InputGroup>
         </Col>
         <Col xs={12} md={12}>
@@ -215,18 +216,20 @@ const updateStatus = (value, index, id) => {
           </div>
         </Col>
       </Row>
-      {
-        orders.map((o, index) =>
-          <div key={index} className="m-2 mb-4">
-            <Card className="m-2" style={{ background: colorBadge[o.status.name] }}>
-              <Card.Header className="row">
-                <div className="col-3">
-                  <div style={{ color: "black" }}>ID: {o._id}</div>
-                </div>
-                <div className="col-1">
-                  <Badge bg={effectBadge[o.status.name]}>{o.status.name}</Badge>
-                </div>
-                <div className="col-3">
+      {orders.map((o, index) => (
+        <div key={index} className="m-2 mb-4">
+          <Card
+            className="m-2"
+            style={{ background: colorBadge[o.status.name] }}
+          >
+            <Card.Header className="row">
+              <div className="col-3">
+                <div style={{ color: "black" }}>ID: {o._id}</div>
+              </div>
+              <div className="col-1">
+                <Badge bg={effectBadge[o.status.name]}>{o.status.name}</Badge>
+              </div>
+              <div className="col-3">
                 <InputGroup>
                   <InputGroup.Text style={{ backgroundColor: "#008DDA" }}>
                     Change status
@@ -243,31 +246,46 @@ const updateStatus = (value, index, id) => {
                   </Form.Select>{" "}
                 </InputGroup>
               </div>
-                <div className="col-3">
-                  <p style={{ color: "black" }}>Create date: {new Date(o.date).toLocaleString()}</p>
-                </div>
-
-              </Card.Header>
-              <Card.Body className="row m-1" style={{ background: "white" }}>
-                <Card.Title className="col-3">
-                  {o.name} <br></br>
-                  (+1) {o.phone}
-                </Card.Title>
-                <Card.Text className="col-5">
-                  {'zipcode: ' + o.address?.zipcode + ', ' + o.address?.detailAddress + ', ' + o.address?.city + ", " + o.address?.state + ', ' + o.address?.country}
-                </Card.Text>
-                <Card.Text className="col-2">
-                  {o.productList?.length} x products
-                </Card.Text>
-                <Card.Text className="col-2">
-                  $ {o.totalAmount}
-                </Card.Text>
-                <Button onClick={() => { setLgShow(true); openDetail(index); }} className="btn-light btn-outline-dark">Detail</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        )
-      }
+              <div className="col-3">
+                <p style={{ color: "black" }}>
+                  Create date: {new Date(o.date).toLocaleString()}
+                </p>
+              </div>
+            </Card.Header>
+            <Card.Body className="row m-1" style={{ background: "white" }}>
+              <Card.Title className="col-3">
+                {o.name} <br></br>
+                (+1) {o.phone}
+              </Card.Title>
+              <Card.Text className="col-5">
+                {"zipcode: " +
+                  o.address?.zipcode +
+                  ", " +
+                  o.address?.detailAddress +
+                  ", " +
+                  o.address?.city +
+                  ", " +
+                  o.address?.state +
+                  ", " +
+                  o.address?.country}
+              </Card.Text>
+              <Card.Text className="col-2">
+                {o.productList?.length} x products
+              </Card.Text>
+              <Card.Text className="col-2">$ {o.totalAmount}</Card.Text>
+              <Button
+                onClick={() => {
+                  setLgShow(true);
+                  openDetail(index);
+                }}
+                className="btn-light btn-outline-dark"
+              >
+                Detail
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
       <Modal
         size="lg"
         show={lgShow}
@@ -281,14 +299,30 @@ const updateStatus = (value, index, id) => {
         </Modal.Header>
         <Modal.Body>
           <Card className="m-2">
-            <Card.Header style={{ background: colorBadge[currentDetail.status?.name] }}><div style={{ color: "black" }}>Status</div> <Badge bg={effectBadge[currentDetail.status?.name]}>{status[currentDetail.status]?.name}</Badge></Card.Header>
+            <Card.Header
+              style={{ background: colorBadge[currentDetail.status?.name] }}
+            >
+              <div style={{ color: "black" }}>Status</div>{" "}
+              <Badge bg={effectBadge[currentDetail.status?.name]}>
+                {status[currentDetail.status]?.name}
+              </Badge>
+            </Card.Header>
             <Card.Body>
               <Card.Title>
                 {currentDetail.name} <br></br>
                 (+1) {currentDetail.phone}
               </Card.Title>
               <Card.Text>
-                {'zipcode: ' + currentDetail.address?.zipcode + ', ' + currentDetail.address?.detailAddress + ', ' + currentDetail.address?.city + ", " + currentDetail.address?.state + ', ' + currentDetail.address?.country}
+                {"zipcode: " +
+                  currentDetail.address?.zipcode +
+                  ", " +
+                  currentDetail.address?.detailAddress +
+                  ", " +
+                  currentDetail.address?.city +
+                  ", " +
+                  currentDetail.address?.state +
+                  ", " +
+                  currentDetail.address?.country}
               </Card.Text>
               <Card.Text>
                 {currentDetail.productList?.length} x products
@@ -305,21 +339,34 @@ const updateStatus = (value, index, id) => {
                     </tr>
                   </thead>
                   <tbody>
-                    {currentDetail?.productList?.map((p, index) =>
+                    {currentDetail?.productList?.map((p, index) => (
                       <tr key={index}>
                         <td>{index + 1}</td>
-                        <td><button onClick={() => { window.location = `/product/${p.productId}`; }} type='button' style={{ minWidth: "10ch" }} className='btn btn-dark'>{p.productId} <AiFillCaretRight className='m-0' /></button></td>
+                        <td>
+                          <button
+                            onClick={() => {
+                              window.location = `/product/${p.productId}`;
+                            }}
+                            type="button"
+                            style={{ minWidth: "10ch" }}
+                            className="btn btn-dark"
+                          >
+                            {p.productId} <AiFillCaretRight className="m-0" />
+                          </button>
+                        </td>
                         <td>{p.productName}</td>
                         <td>{p.quantity}</td>
                         <td>{Number(p.unitPrice) * p.quantity}</td>
                       </tr>
-                    )}
+                    ))}
                   </tbody>
                 </Table>
               </Card.Text>
               <Card.Text>
                 <p style={{ fontWeight: "bold" }}>Total:</p>
-                <p style={{ fontWeight: "bold" }}>$ {currentDetail.totalAmount}</p>
+                <p style={{ fontWeight: "bold" }}>
+                  $ {currentDetail.totalAmount}
+                </p>
               </Card.Text>
             </Card.Body>
           </Card>
