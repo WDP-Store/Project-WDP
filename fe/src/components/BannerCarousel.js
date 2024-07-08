f38dfb7a76c40735efd23bbbb522b67573c4c986
toanbdhe161559 <buiductoanfu@gmail.com>
2024-07-08 17:24:39 +0700

    homepageUi

--- fe/src/components/BannerCarousel.js
+++ fe/src/components/BannerCarousel.js
@@ -1,31 +1,31 @@
 import Carousel from "react-bootstrap/Carousel";
 const BannerCarousel = () => {
-  return (
-    <Carousel data-bs-theme="dark" interval={1500}>
-      <Carousel.Item style={{ height: "20rem" }}>
-        <img
-          className="d-block w-100"
-          src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/03/banner/y36-720-220-720x220.png"
-          // src="https://new-ella-demo.myshopify.com/cdn/shop/files/home-19-banner-custom-4_570x.jpg?v=1645072864"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/02/banner/reno11-720-220-720x220-2.png"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/02/banner/iPhone-15-Pro-Max-720-220-720x220-4.png"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-  );
+    return (
+        <Carousel data-bs-theme="dark" controls={false} indicators={false} interval={2000}>
+            <Carousel.Item style={{ height: "20rem" }}>
+                <img
+                    className="d-block w-100"
+                    src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/03/banner/y36-720-220-720x220.png"
+                    // src="https://new-ella-demo.myshopify.com/cdn/shop/files/home-19-banner-custom-4_570x.jpg?v=1645072864"
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/02/banner/reno11-720-220-720x220-2.png"
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/02/banner/iPhone-15-Pro-Max-720-220-720x220-4.png"
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
+    );
 };
 
 export default BannerCarousel;
--- fe/src/components/ProductCarouselV1.js
+++ fe/src/components/ProductCarouselV1.js
@@ -1,25 +1,25 @@
 import Carousel from "react-bootstrap/Carousel";
-import ProductItem from './ProductItem';
+import ProductItem from "./ProductItem";
 const ProductCarouselV1 = ({ products }) => {
-  const slice4Products = [];
-  for (let i = 0; i < products.length; i += 4) {
-    slice4Products.push(products.slice(i, i + 4));
-  }
-  return (
-    <Carousel data-bs-theme="dark" interval={1500}>
-      {slice4Products.map((chunk, index) => (
-        <Carousel.Item key={index}>
-          <div className="row">
-            {chunk.map((p, index) => (
-              <div key={index} className="col-3">
-                <ProductItem product={p} />
-              </div>
+    const slice4Products = [];
+    for (let i = 0; i < products.length; i += 4) {
+        slice4Products.push(products.slice(i, i + 4));
+    }
+    return (
+        <Carousel data-bs-theme="dark" interval={1500} indicators={false}>
+            {slice4Products.map((chunk, index) => (
+                <Carousel.Item key={index}>
+                    <div className="row">
+                        {chunk.map((p, index) => (
+                            <div key={index} className="col-3">
+                                <ProductItem product={p} />
+                            </div>
+                        ))}
+                    </div>
+                </Carousel.Item>
             ))}
-          </div>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  );
+        </Carousel>
+    );
 };
 
 export default ProductCarouselV1;
